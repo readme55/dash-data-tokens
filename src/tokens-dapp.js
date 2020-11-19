0990f4bf8deb652b8787bd7683faed07247b4a12
readme <readme55@protonmail.com>
2020-11-19 17:49:27 +0100

    update

--- src/tokens-dapp.js
+++ src/tokens-dapp.js
@@ -1,10 +1,31 @@
 // v1 4th: 946mB3VPhRCBNhJbXGvuR9YqpHaXPbUr3GvpqsKTnSN7
-// v2 5th: 
+// v2 5th: 3jLjo5FGFQVauSVYTSFqMozrUxjBENzFXciFQSY4AQQG
 
 // cloud identID: AfZxsSWVKxDpHkXHQDqhEbyZYmfNcAgKq76TLCab4ZiD
 // dappuser identID: 72xw6JyFKeRjMBNJpEU6vaq9oCpmTMi5dEF7jenN3Btp
 
 $(document).ready(function () {
+
+    // test mass documents in browser, result: 3sec for 100M values
+    // var massLen = 100000000;
+    // console.log("creating data set with " + massLen + " values")
+    // var start = new Date().getTime();   // Remember when we started
+    // var mass = [];
+    // var result = [];
+    // var rand = 0;
+    // for (var i = 0; i < massLen; i++) {
+    //     rand =  Math.floor(Math.random() * 10);
+    //     mass.push(rand);
+    //     // console.log(rand)
+    // }
+    // console.log("finish creating dataset, processing now")
+    // for (var i = 0; i < massLen; i++) {
+    //     if (mass[i] == 1) result.push(mass[i]);
+    // }
+    // var end = new Date().getTime(); // Remember when we finished
+    // console.log(end - start);   // Now calculate and output the difference
+    // console.log("finish processing, found " + result.length + " matching values")
+
 
     var username = sessionStorage.getItem('dash_username');
     var identityId = sessionStorage.getItem('dash_identityID');
@@ -34,12 +55,12 @@ $(document).ready(function () {
             $("#formTokenDecimals").prop('readonly', false);
         } else {    // if filled
             $("#createBtn").prop('disabled', true);
-            $("#initBtn").prop('disabled', true);
+            // $("#initBtn").prop('disabled', true);
             $("#receiveBtn").prop('disabled', false);
             $("#sendBtn").prop('disabled', true);
             $("#formTokenName").prop('readonly', true);
             $("#formTokenSymbol").prop('readonly', true);
-            $("#formTokenAmount").prop('readonly', true);
+            // $("#formTokenAmount").prop('readonly', true);
             $("#formTokenDecimals").prop('readonly', true);
         }
     });
