bba0863a6f19076675cbefefc11aa6eb8516db8a
readme <readme55@protonmail.com>
2021-02-21 12:14:08 +0100

    update

--- src/credentials.js
+++ src/credentials.js
@@ -2,6 +2,5 @@ const dappMnemonic = 'era message genuine crystal margin cricket fabric address 
 const dappIdentityId = 'HK1jnhxWMDuoEwkvKXTXFR5WeTYzx4Vm9C7Yx9qPhWXC';
 const dappAddress = 'yMxELPfsPbn5UienyykyySLJ4TA3QQCqw5'
 const messageContractId = '3coTShwTvmKiySpwmDDzRJ9FGQx4RNLdawLttT9RNxXa';
-// const noteContractId = '9PiTpFPE4BC9Ugo2etsD2UDntv37qgHWoj67oW8fWhP6';
 const dpnsContractID = '36ez8VqoDbR8NkdXwFaf9Tp8ukBdQxN8eYs8JNMnUyKz';
 const dashNetwork = 'testnet';
