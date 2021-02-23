69f4861d6cc4b9aeff66a2e27adbd5398bc2253e
readme <readme55@protonmail.com>
2021-02-23 11:59:39 +0100

    add tests

--- src/eval/test.js
+++ src/eval/test.js
@@ -1,4 +1,4 @@
-"use strict"
+// "use strict"
 
 let submitBtn = document.getElementById('submitBtn');
 let submitText = document.getElementById('submitText');
@@ -9,89 +9,291 @@ let submitText = document.getElementById('submitText');
 // assert(typeof testBigInt === "BigInt");
 // assert(typeof testString === "string");
 
-submitBtn.addEventListener('click', function () {
-    console.log("click")
-    submitBtn.disabled = true;
-  
-    const clientOpts = {
-      network: dashNetwork,
-      wallet: {
-        mnemonic: dappMnemonic,
-        adapter: localforage
+submitBtn.addEventListener('click', async function () {
+  console.log("click")
+  submitBtn.disabled = true;
+
+  client.getApps().set("tokenContract", { "contractId": funToken });
+  const platform = client.platform;
+
+  let tokenContractId = funToken;
+
+  let balance;
+  let tName;
+  let tSymbol;
+  let tDecimals;
+  let tRecipient;
+  let tAmount;
+  let docProperties;
+  let documentBatch;
+  let testDoc;
+
+  let origBalance;
+  let validAmount = 100000000n;
+
+  const submitTestDocuments = async function () {
+
+    try {
+      const identity = await client.platform.identities.get(dappIdentityId);
+
+      // test 1
+      console.log("----------run test 1: valid tx amount 1")
+      await processDocumentChain(tokenContractId, dappIdentityId);
+
+      balance = getAccBalance();
+      origBalance = balance;
+      tName = name();
+      tSymbol = symbol();
+      tDecimals = parseInt(decimals());
+
+      console.log("Current Balance: " + balance)
+
+      tRecipient = '5uvhMEpiCDLYA2oqTq3WHcxMb1QJQKMeYFSfFisuPFdE';  // readme
+      tAmount = validAmount.toString();
+
+      docProperties = createValidTx(tName, tSymbol, tDecimals, tRecipient, tAmount, balance.toString(), 'amount 1 token')
+
+      documentBatch = {
+        create: [],
+        replace: [],
+        delete: [],
       }
-    };
-    const client = new Dash.Client(clientOpts);
-  
-    client.getApps().set("tokenContract", { "contractId": 'DY6KmhAsLqrkTxJWA7KAJA3vR4wHhExHqSYXLWitdxuu' });
-    const platform = client.platform;
-    
-  
-    const submitNoteDocument = async function () {
-  
-      try {
-        // const account = await client.getWalletAccount();
-        const identity = await client.platform.identities.get(dappIdentityId);
-  
-        docProperties = {
-            version: 1,
-            name: 'Evaluation',
-            symbol: 'eva5',
-            decimals: 8,
-            sender: dappIdentityId,   
-            recipient: dappIdentityId,    // dapp login user identityId
-            amount: '1',
-            data: 'test evaluation',
-            owner: dappIdentityId,
-            balance: '1000',
-            lastValIndTransfer: 0,
-            lastValIndTransferFrom: 0
-        }
-    
-        var documentBatch = {
-          create: [],
-          replace: [],
-          delete: [],
-        }
-  
-        var start = new Date().getTime();
-  
-        for (m = 0; m < 20; m++) {
-          console.log("ROUND " + m)
-  
-          for (i = 0; i < 3; i++) {
-  
-            let noteDocument1 = await platform.documents.create(
-              'tokenContract.token',
-              identity,
-              docProperties,
-            );
-  
-            // console.log(documentBatch['create'][0].data.symbol)
-            documentBatch['create'][i] = noteDocument1;
-            // documentBatch['replace'][i] = '';
-            // documentBatch['delete'][i] = '';
-          }
-  
-          // get size
-          console.log(sizeOf(documentBatch))
-  
-          // Sign and submit the document
-          await platform.documents.broadcast(documentBatch, identity);
-          console.log("fin")
-        }
-        var end = new Date().getTime();
-        var time = end - start;
-        console.log("RUNTIME " + time)
-  
-      } catch (e) {
-        console.error('Something went wrong:', e);
-      } finally {
-        console.log("submited login document with message: " + submitText.value)
-        // client.disconnect();
+
+      testDoc = await platform.documents.create(
+        'tokenContract.token',
+        identity,
+        docProperties,
+      );
+
+      documentBatch['create'][0] = testDoc;
+
+      await platform.documents.broadcast(documentBatch, identity);
+
+
+      // test 2
+      console.log("----------run test 2: amount bigger balance")
+      await processDocumentChain(tokenContractId, dappIdentityId);
+
+      balance = getAccBalance();
+      tName = name();
+      tSymbol = symbol();
+      tDecimals = decimals();
+
+      console.log("Current Balance: " + balance)
+
+      tRecipient = '5uvhMEpiCDLYA2oqTq3WHcxMb1QJQKMeYFSfFisuPFdE';  // readme
+      tAmount = '100000000000000';
+
+      docProperties = createInvalidTx(tName, tSymbol, tDecimals, dappIdentityId, tRecipient, tAmount, dappIdentityId, balance.toString(), 'amount bigger balance')
+
+      documentBatch = {
+        create: [],
+        replace: [],
+        delete: [],
       }
-    };
-    submitNoteDocument();
-    submitBtn.disabled = false;
-    console.log("done")
-  
-  }, false);
+
+      testDoc = await platform.documents.create(
+        'tokenContract.token',
+        identity,
+        docProperties,
+      );
+
+      documentBatch['create'][0] = testDoc;
+
+      await platform.documents.broadcast(documentBatch, identity);
+
+
+      // test 3
+      // console.log("run test 3: negative amount")
+      // await processDocumentChain(tokenContractId, dappIdentityId);
+
+      // balance = getAccBalance();
+      // tName = name();
+      // tSymbol = symbol();
+      // tDecimals = decimals();
+      
+      // console.log("Current Balance: " + balance)
+
+      // // tRecipient = '5uvhMEpiCDLYA2oqTq3WHcxMb1QJQKMeYFSfFisuPFdE';  // readme
+      // tAmount = '-100000000';
+
+      // docProperties = invalidTx(tName, tSymbol, tDecimals, dappIdentityId, tRecipient, tAmount, dappIdentityId, balance.toString(), 'negative amount')
+
+      // documentBatch = {
+      //   create: [],
+      //   replace: [],
+      //   delete: [],
+      // }
+
+      // testDoc = await platform.documents.create(
+      //   'tokenContract.token',
+      //   identity,
+      //   docProperties,
+      // );
+
+      // documentBatch['create'][0] = testDoc;
+
+      // await platform.documents.broadcast(documentBatch, identity);
+
+
+      // test 4
+      console.log("----------run test 4: Send zero amount tx")
+      await processDocumentChain(tokenContractId, dappIdentityId);
+
+      balance = getAccBalance();
+      tName = name();
+      tSymbol = symbol();
+      tDecimals = decimals();
+      
+      console.log("Current Balance: " + balance)
+
+      tRecipient = '5uvhMEpiCDLYA2oqTq3WHcxMb1QJQKMeYFSfFisuPFdE';  // readme
+      tAmount = '0';
+
+      docProperties = createValidTx(tName, tSymbol, tDecimals, tRecipient, tAmount, balance.toString(), 'zero amount')
+
+      documentBatch = {
+        create: [],
+        replace: [],
+        delete: [],
+      }
+
+      testDoc = await platform.documents.create(
+        'tokenContract.token',
+        identity,
+        docProperties,
+      );
+
+      documentBatch['create'][0] = testDoc;
+
+      await platform.documents.broadcast(documentBatch, identity);
+
+
+
+      // test 5
+      console.log("----------run test 5: wrong sender")
+      await processDocumentChain(tokenContractId, dappIdentityId);
+
+      balance = getAccBalance();
+      tName = name();
+      tSymbol = symbol();
+      tDecimals = decimals();
+      
+      console.log("Current Balance: " + balance)
+
+      tRecipient = '5uvhMEpiCDLYA2oqTq3WHcxMb1QJQKMeYFSfFisuPFdE';  // readme
+      tAmount = validAmount.toString();
+
+      docProperties = createInvalidTx(tName, tSymbol, tDecimals, '5uvhMEpiCDLYA2oqTq3WHcxMb1QJQKMeYFSfFisuPFdE', tRecipient, tAmount, dappIdentityId, balance.toString(), 'wrong sender')
+
+      documentBatch = {
+        create: [],
+        replace: [],
+        delete: [],
+      }
+
+      testDoc = await platform.documents.create(
+        'tokenContract.token',
+        identity,
+        docProperties,
+      );
+
+      documentBatch['create'][0] = testDoc;
+
+      await platform.documents.broadcast(documentBatch, identity);
+
+
+      // test 6
+      console.log("----------run test 6: mint tokens from zero-address")
+      await processDocumentChain(tokenContractId, dappIdentityId);
+
+      balance = getAccBalance();
+      tName = name();
+      tSymbol = symbol();
+      tDecimals = decimals();
+      
+      console.log("Current Balance: " + balance)
+
+      // tRecipient = '5uvhMEpiCDLYA2oqTq3WHcxMb1QJQKMeYFSfFisuPFdE';  // readme
+      tAmount = validAmount.toString();
+
+      docProperties = createInvalidTx(tName, tSymbol, tDecimals, '1'.repeat(42), tRecipient, tAmount, dappIdentityId, balance.toString(), 'mint tokens')
+
+      documentBatch = {
+        create: [],
+        replace: [],
+        delete: [],
+      }
+
+      testDoc = await platform.documents.create(
+        'tokenContract.token',
+        identity,
+        docProperties,
+      );
+
+      documentBatch['create'][0] = testDoc;
+
+      await platform.documents.broadcast(documentBatch, identity);
+
+
+
+      // test 7
+      console.log("----------run test 7: duplicate tx")
+      await processDocumentChain(tokenContractId, dappIdentityId);
+
+      balance = origBalance;
+      tName = name();
+      tSymbol = symbol();
+      tDecimals = decimals();
+      
+      console.log("Current Balance: " + balance)
+
+      // tRecipient = '5uvhMEpiCDLYA2oqTq3WHcxMb1QJQKMeYFSfFisuPFdE';  // readme
+      tAmount = validAmount.toString();
+
+      docProperties = createInvalidTx(tName, tSymbol, tDecimals, dappIdentityId, tRecipient, tAmount, dappIdentityId, balance.toString(), 'duplicate')
+
+      documentBatch = {
+        create: [],
+        replace: [],
+        delete: [],
+      }
+
+      testDoc = await platform.documents.create(
+        'tokenContract.token',
+        identity,
+        docProperties,
+      );
+
+      documentBatch['create'][0] = testDoc;
+
+      await platform.documents.broadcast(documentBatch, identity);
+
+      testComplete = true;
+
+
+    } catch (e) {
+      console.error('Something went wrong:', e);
+      console.log('Testing aborted')
+      fail;
+    } finally {
+      console.log("submited login document with message: " + submitText.value)
+      // client.disconnect();
+    }
+  };
+  await submitTestDocuments();
+  submitBtn.disabled = false;
+  console.log("done")
+
+  balance = getAccBalance();
+
+  console.log("Finish testing!")
+  console.log("Original balance: " + origBalance)
+  console.log("Final balance " + balance)
+  if(origBalance - validAmount === balance) {
+    console.log("Testing successful")
+  } else {
+    console.log("Testing un-successful! Check logs")
+  }
+
+}, false);
